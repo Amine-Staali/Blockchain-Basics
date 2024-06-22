d394f15ada7f44456863dbef74e6ed76920b603e
Staali Mohamed Amine <138203471+Amine-Staali@users.noreply.github.com>
2024-06-22 17:11:08 +0100

    Update settings.py

--- Blockchain/Blockchain/settings.py
+++ Blockchain/Blockchain/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tsez^0n!+wvou($*=fsq8%k3c=75!wadd5cwgn96g_=%7!kjd$'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -79,8 +79,8 @@ WSGI_APPLICATION = 'Blockchain.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Blockchain',
-        'USER': 'postgres',
+        'NAME': '',
+        'USER': '',
         'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432',
